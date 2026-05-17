28c4c26bee976a5d1bf8c547aeef75057f50ecc0
lab-ese <starktonymk85@gmail.com>
2026-05-17 21:36:05 +0530

    uploaded via file upload

--- 10_chisq_independence.R
+++
@@ -1,8 +0,0 @@
-# Chi-square test for independence - E-commerce
-df <- read.csv("datasets/ecommerce.csv")
-contingency <- table(df$Category, df$Payment)
-result <- chisq.test(contingency)
-print(result)
-
-dev.new()
-mosaicplot(contingency, color=TRUE, main="Category vs Payment")
--- 11_function_ifelse.R
+++
@@ -1,7 +0,0 @@
-# Function to compute average + if-else classification
-avg <- function(x) sum(x)/length(x)
-data <- c(10,20,30,40,50)
-cat("Average:", avg(data), "\n")
-
-num <- -5
-if(num > 0) cat("Positive\n") else if(num < 0) cat("Negative\n") else cat("Zero\n")
